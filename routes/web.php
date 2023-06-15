46d2a21f5937949a63d34f58922d43ecc6846546
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-15 19:29:30 +0900

    質問投稿編集機能実装

--- routes/web.php
+++ routes/web.php
@@ -45,6 +45,10 @@ Route::group(["middleware" => ["auth"]], function() {
     Route::get("/posts/{post}",[PostController::class, "show"]);
 
     Route::post("/posts", [PostController::class, "store"]);
+
+    Route::get("/posts/{post}/edit", [PostController::class, "edit"]);
+
+    Route::put("/posts/{post}", [PostController::class, "update"]);
 });
 
 require __DIR__.'/auth.php';
