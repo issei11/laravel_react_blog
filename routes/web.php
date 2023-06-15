697d281f119ca3c4ede0167da8662be956c91275
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-15 16:38:09 +0900

    質問投稿一覧実装

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\PostController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
@@ -33,6 +34,13 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+
+Route::group(["middleware" => ["auth"]], function() {
+    Route::get("/posts", [PostController::class, "index"]);
+
+    Route::get("/posts/{post}",[PostController::class, "show"]);
 });
 
 require __DIR__.'/auth.php';
