70f83b09c58fa3d2a1d121ea0c873746dd1faf8d
issei11 <isseim1129@g.ecc.u-tokyo.ac.jp>
2023-06-20 13:49:59 +0900

    質問投稿削除機能実装

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,8 @@ Route::group(["middleware" => ["auth"]], function() {
     Route::get("/posts/{post}/edit", [PostController::class, "edit"]);
 
     Route::put("/posts/{post}", [PostController::class, "update"]);
+
+    Route::delete("/posts/{post}", [PostController::class, "delete"]);
 });
 
 require __DIR__.'/auth.php';
